07e7e225efd8ba3e30ac138792a0bfa383b08b97
N Nalini <122100742+nnalinireddy@users.noreply.github.com>
2023-01-06 19:15:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,5 @@ import streamlit
 
 streamlit.title ('First Test in Github')
 
-('Jan 6th I made first attempt')
+streamlit.header('First Test Detail')
+streamlit.text('Jan 6th I made first attempt')
